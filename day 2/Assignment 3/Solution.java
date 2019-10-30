51b93c583112156dd2d8f916820b45642a3d18e7
saragadamraviteja <saragadamraviteja@msitprogram.net>
2019-10-30 14:12:21 +0530

    final

---
+++ day 2/Assignment 3/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution{
+	public static String isMatching(String str){
+Stack<Character> x = new Stack<Character>();
+    for (int i = 0; i < str.length(); i++) {
+        Character ch = str.charAt(i);
+        if (str.charAt(i) == '{' || str.charAt(i) == '['
+                || str.charAt(i) == '(') {
+            x.push(str.charAt(i));
+        } else {
+            if (x.isEmpty() 
+                    || (x.peek() == '[' && ch == '[')
+                    || (x.peek() == '{' && ch == '{')
+                    || (x.peek() == '(' && ch == '(')) {
+                return "NO";
+            } else {
+                x.pop();
+            }
+        }
+    }
+    if (x.empty())
+        return "YES";
+    return "NO";
+}
+}
---
+++ day 2/Assignment 4/Solution.java
@@ -0,0 +1,7 @@
+class Solution{
+	public static String Josephus(int a, int b){
+		// fill you code Here
+		
+		return "";
+	}
+}
