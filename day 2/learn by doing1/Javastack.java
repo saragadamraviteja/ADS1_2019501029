d8436cebcc641906d3b721a175eceec76269eb69
saragadamraviteja <saragadamraviteja@msitprogram.net>
2019-10-31 13:32:39 +0530

    final

--- day 2/learn by doing1/Javastack.java
+++ day 2/learn by doing1/Javastack.java
@@ -1,9 +1,19 @@
 import java.util.*;
-
-public class Javastack {
-    public static void main(String [] args) {
-        String s = "it was - the best - of times - - - it was - the - -";
-        String [] arr = s.split(" ");
+/**
+ * In This class we are using stack and performing operations
+ * and methods like pop, push.
+ */
+public final class Javastack {
+    private Javastack() {
+        // for check style
+    }
+    /**
+     * this method uses the pop and push methods from the stack class
+     * and displays the string output.
+     * @param s input string.
+     */
+    public static void java(final String s) {
+        String[] arr = s.split(" ");
         Stack1 obj = new Stack1(arr.length);
         for (int i = 0; i < arr.length; i++) {
             if (arr[i].equals("-")) {
@@ -12,13 +22,21 @@ public class Javastack {
                  obj.push(arr[i]);
              }
         }
-        String [] disp = obj.getarray();
-        // System.out.println(disp);
-        // System.out.println(Arrays.toString(obj.getarray()));
+        String[] disp = obj.getarray();
         for (int j = 0; j < disp.length; j++) {
             if (disp[j] != null) {
                System.out.println(disp[j]);
             }
         }
+
+    }
+    /**
+     * The actual program starts with this main method and calls java method
+     * for further operations.
+     * @param args String arguements.
+     */
+    public static void main(final String[] args) {
+        String s = "it was - the best - of times - - - it was - the - -";
+        java(s);
     }
 }
