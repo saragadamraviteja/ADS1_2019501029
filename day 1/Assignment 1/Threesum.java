c7cfcba0d6c9f3ac9d294ad305a3f1614bdde726
saragadamraviteja <saragadamraviteja@msitprogram.net>
2019-11-05 13:26:38 +0530

    final

--- day 1/Assignment 1/Threesum.java
+++ day 1/Assignment 1/Threesum.java
@@ -7,18 +7,18 @@ public class ThreeSum {
     /**
      * integer count stores the count value.
     */
-    private int count = 0;
+    //private static int count = 0;
     /**
      * yy is used to store the sum of the two values.
      */
-    private int yy;
+    //private static int yy;
     /**
      *
      * @param array array of integers it stores.
      * @param x key value it stores.
      * @return it returns the mid value value of the array.
      */
-    public int  binary(final int[] array, final int x) {
+    public static int  binary(final int[] array, final int x) {
         int low = 0;
         int high = array.length - 1;
         while (low <= high) {
@@ -40,21 +40,22 @@ public class ThreeSum {
      */
     public static void threesum(final int[] array) {
         Arrays.sort(array);
-        ThreeSum obj = new ThreeSum();
+        // ThreeSum obj = new ThreeSum();
+        int count = 0;
         for (int j = 0; j < array.length; j++) {
             for (int k = j + 1; k < array.length; k++) {
-                obj.yy =  -(array[j] + array[k]);
-                   int dd = obj.binary(array, obj.yy);
-                   if (dd == obj.yy) {
+                int yy =  -(array[j] + array[k]);
+                   int dd = binary(array, yy);
+                   if (dd == yy) {
                     if (array[j] < array[k]) {
                         if (array[k] < dd) {
-                            obj.count += 1;
+                            count += 1;
                         }
                     }
                    }
             }
         }
-        System.err.println(obj.count);
+        System.err.println(count);
     }
 /**
  * this is the main method where the program starts.
