3be43aa9132f82c3c7021d4ae3e20982f49ee3a3
saragadamraviteja <saragadamraviteja@msitprogram.net>
2019-11-08 18:04:50 +0530

    final

--- day 7/Assignment 1/Queue.java
+++ day 7/Assignment 1/Queue.java
@@ -1,20 +1,43 @@
 import java.util.Iterator;
+/**
+ * Queue class.
+ * @param <Item> generic.
+ */
 public class Queue<Item> implements Iterable<Item> {
+    /**
+     * first stores element.
+     */
     private Node<Item> first;
+    /**
+     * last stores element.
+     */
     private Node<Item> last;
+    /**
+     * it stores the size.
+     */
     private int size;
+
     private static class Node<Item> {
         private Item item;
         private Node<Item> next;
     }
+    /**
+     * Queue constructor for fields.
+     */
     public Queue() {
         first = null;
         last = null;
         size = 0;
     }
+    /**
+     * size of the list
+     */
     public int size() {
         return size;
     }
+    /**
+     * adding elements to the list.
+     */
     public void Enqueue(Item item) {
         Node<Item> oldlast = last;
         last = new Node<Item>();
@@ -27,6 +50,9 @@ public class Queue<Item> implements Iterable<Item> {
         }
         size++;
     }
+    /**
+     * removing elements from the list.
+     */
     public Item Dequeue() {
         if (isEmpty()) return null;
         Item item = first.item;
@@ -35,9 +61,15 @@ public class Queue<Item> implements Iterable<Item> {
         if (isEmpty()) last = null;
         return item;
     }
+    /**
+     * checking list is empty or not.
+     */
     public boolean isEmpty() {
         return first == null;
     }
+    /**
+     * to string method.
+     */
     public String toString() {
         StringBuilder s = new StringBuilder();
         for (Item item : this) {
@@ -46,12 +78,20 @@ public class Queue<Item> implements Iterable<Item> {
         }
         return s.toString();
     }
+    /**
+     * iterator 
+     */
     public Iterator<Item> iterator() {
         return new ListIterator();
     }
+    /**
+     * Iterator.
+     */
     private class ListIterator implements Iterator<Item> {
         Node<Item> current = first;
-
+        /**
+         * has next method
+         */
         public boolean hasNext() {
             return current != null;
         }
