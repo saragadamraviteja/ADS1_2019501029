2c9958e876a1bb1d1ae27085f27f13766f99c89b
saragadamraviteja <saragadamraviteja@msitprogram.net>
2019-11-14 22:28:35 +0530

    final

--- project 1/RandomizedQueue.java
+++ project 1/RandomizedQueue.java
@@ -2,6 +2,7 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import edu.princeton.cs.algs4.StdOut;
 import java.util.Random;
+import java.util.Arrays;
 /**
  * RandomizedQueue
  */
@@ -12,7 +13,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // construct an empty randomized queue
     public RandomizedQueue() {
-        arr = new Item[4];
+        arr = (Item[]) new Object[150];
         size = 0;
     }
 
@@ -28,7 +29,21 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // add the item
     public void enqueue(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        if (size >= 0.5 * (arr.length)) {
+            resize();
+        } 
         arr[size++] = item;
+    }
+
+    public void resize() {
+        if (size >= 0.5 *(arr.length)) {
+        arr = Arrays.copyOf(arr, 2 * arr.length);
+        } else if (size <= 0.25 * (arr.length)) {
+            arr = Arrays.copyOf(arr, (1/2) * arr.length);
+        }
     }
 
     // remove and return a random item
@@ -38,23 +53,58 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         }
         Random rand = new Random();
         int r = rand.nextInt(size);
+        Item tempo = arr[r]; 
         arr[r] = null;
         for (int i = r; i < size;i++) {
             arr[i] = arr[i+1];
         }
         arr[size] = null;
         size--;
+        return tempo;
     }
 
     // return a random item (but do not remove it)
     public Item sample() {
-        
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException();
+        }
+        Random rnd = new Random();
+        int t = rnd.nextInt(size);
+        return arr[t];
     }
 
-    // return an independent iterator over items in random order
-    public Iterator<Item> iterator()
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private int i;
+        public boolean hasNext() {
+            return i < size;
+        }
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = arr[i];
+            i++;
+            return item;
+        }
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
 
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        RandomizedQueue<String> objj = new RandomizedQueue();
+        objj.enqueue("ravi");
+        objj.enqueue("teja");
+        objj.enqueue("good boy");
+        objj.dequeue();
+        objj.size();
+        objj.isEmpty();
+        for (String string : objj) {
+            StdOut.println(string);
+        }
+    }
 
 }
