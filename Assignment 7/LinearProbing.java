b5eb9fc4c7b84fbc1974f5307b1679be90fd9c83
saragadamraviteja <saragadamraviteja@msitprogram.net>
2019-11-21 16:18:14 +0530

    final

--- Assignment 7/LinearProbing.java
+++ Assignment 7/LinearProbing.java
@@ -1,99 +1,91 @@
-/**
- * Linear probing implementation.
- * @author raviteja.
- */
-import java.util.Arrays;
 import java.util.*;
 public class LinearProbing<Key, Value> {
-    /**
-     * which stores the keys in an array
-     */
-    Key[] keys;
-    /**
-     * which stores an values in array.
-     */
-    Value[] values;
-    /**
-     * linear probing constructor for fields.
-     */
+    private static final int INIT_CAPACITY = 4;
+
+    private int n;           // number of key-value pairs in the symbol table
+    private int m;           // size of linear probing table
+    private Key[] keys;      // the keys
+    private Value[] vals;    // the values
+
     public LinearProbing() {
-        keys = (Key[]) new Object[1000000];
-        values = (Value[]) new Object[1000000];
-        // createDict(ks, vs);
+        this(INIT_CAPACITY);
     }
-    /**
-     * for generating hash code.
-     */
-    public int hash(Key k) {
-        return ((k.hashCode()& 0x7fffffff) % (keys.length));
+
+    public LinearProbing(int capacity) {
+        m = capacity;
+        n = 0;
+        keys = (Key[])   new Object[1000000];
+        vals = (Value[]) new Object[1000000];
     }
-    /**
-     * it inserts an key into an array.
-     */
-    public void put(Key k, Value v) {
-        if (k == null) {
-            return;
-        }
-        int index = hash(k);
+
+    public int size() {
+        return n;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % 1000000;
+    }
+
+    public void put(Key key, Value val) {
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+
+        // if (val == null) {
+        //     delete(key);
+        //     return;
+        // }
+
+        // double table size if 50% full
+        // if (n >= m/2) resize(2*m);
+
         int i;
-        for (i = index; keys[i] != null; i = (i + 1) % keys.length) {
-            if (keys[i].equals(k)) {
-                values[i] = v;
-                return; 
+        for (i = hash(key); keys[i] != null; i = (i + 1) % 1000000) {
+            // System.out.println(keys[i]);
+            if (keys[i].equals(key)) {
+                vals[i] = val;
+                return;
             }
         }
-        keys[i] = k;
-        values[i] = v;
+        keys[i] = key;
+        vals[i] = val;
+        n++;
     }
-    /**
-     * it searches an element in an array.
-     * @param k key
-     * @return value.
-     */
-    public Value get(Key k) {
-        if (k == null) {
-            return null;
-        }
-        int index = hash(k);
-        for (int i = index; keys[i] != null; i = (i + 1) % keys.length) {
-            if (keys[i].equals(k)) {
-                return values[i];
-            }
-        }
+
+    public Value get(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+        for (int i = hash(key); keys[i] != null; i = (i + 1) % m)
+            if (keys[i].equals(key))
+                return vals[i];
         return null;
     }
-
-
-/**
- * main method.
- * @param args arguements.
- */
-float count = 0;
- public static void main(String[] args) {
-     
-     //DecimalFormat df = new DecimalFormat();
-     //df.setMaximumFractionDigits(2);
-     Random rr = new Random();
-     LinearProbing<Integer, Integer> ob = new LinearProbing();
-     int k = 0;
-     while (k < 500000) {
-     int  ss = rr.nextInt(10000);
-     int dd = rr.nextInt(2);
-     ob.put (ss, dd);
-     k++;
-     }
-     //System.out.println(Arrays.toString(ob.keys));
-     int l = 0;
-     while (l < 500000) {
-         System.out.println(ob.get(l));
-         if (ob.get(l) != null) {
-            ob.count++;
-            // System.out.println(ob.count);
-         } 
-         l = l + 100;
-     }
-     System.out.println(ob.count);
-     double d = (double) (ob.count)/1000000;
-     System.out.format("%.5f",d);
- }
+double count;
+    public static void main(String[] args) {
+        Random rr = new Random();
+        LinearProbing<Integer, Integer> ob = new LinearProbing();
+        int k = 0;
+        while (k < 500000) {
+            int ss = rr.nextInt(500000);
+            int dd = rr.nextInt(500000);
+            ob.put(ss, dd);
+            k++;
+            // System.out.println(k);
+        }
+        // System.out.println("here");
+        int l = 0;
+        while (l < 500000) {
+            if (ob.get(l) != null) {
+                ob.count++;
+                // System.out.println(Arrays.toString(ob.keys));
+                // System.out.println(ob.count);
+            }
+            l = l + 100;
+        }
+        System.out.println(ob.count);
+        // double d = ;
+        System.out.printf("%.2f", (ob.count/10000));
+    }
 }
+
